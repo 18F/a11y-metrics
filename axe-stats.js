fba931aba7ba0d82b35c3f2b267ade31d35b0abb
Atul Varma <varmaa@gmail.com>
2017-03-28 17:23:43 -0400

    Convert axe-stats.js to use promises.

--- axe-stats.js
+++ axe-stats.js
@@ -4,33 +4,35 @@ const cache = require('./lib/cache');
 const getWebdriver = require('./lib/webdriver');
 
 function getAxeStats(domain) {
-  return cache.get(['axe', domain], () => getWebdriver().then(driver => {
-    return driver.get(`https://${domain}/`).then(() => {
-      const axe = AxeBuilder(driver);
+  return cache.get(['axe', domain], async function() {
+    const driver = await getWebdriver();
 
-      return new Promise((resolve, reject) => {
-        axe.analyze(results => {
-          resolve(results);
-        });
+    await driver.get(`https://${domain}/`);
+
+    const axe = AxeBuilder(driver);
+
+    return new Promise((resolve, reject) => {
+      axe.analyze(results => {
+        resolve(results);
       });
     });
-  }));
+  });
 }
 
 module.exports = getAxeStats;
 
+async function main() {
+  const websites = require('./websites.json');
+
+  for (website of websites) {
+    console.log(`Obtaining axe-core stats for ${website.domain}.`);
+
+    await getAxeStats(website.domain);
+  }
+}
+
 if (module.parent === null) {
-  let promise = Promise.resolve();
-
-  require('./websites.json').forEach(function(website) {
-    promise = promise.then(() => {
-      console.log(`Obtaining axe-core stats for ${website.domain}.`);
-
-      return getAxeStats(website.domain);
-    });
-  });
-
-  promise.catch(err => {
+  main().catch(err => {
     console.log(err);
     process.exit(1);
   });
