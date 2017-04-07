cb99746330b808016d512bf271c12ee271f1573e
Atul Varma <varmaa@gmail.com>
2017-04-07 10:28:28 -0400

    Increase timeout interval for internet tests.

--- test/axe-stats.test.js
+++ test/axe-stats.test.js
@@ -12,6 +12,9 @@ describe('getAxeStats()', () => {
                       SELENIUM_REMOTE_URL &&
                       SELENIUM_BROWSER) ? it : it.skip;
 
+  // This can take particularly long on Travis.
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+
   internetIt('works on sites w/ CSP like github.com', async () => {
     const s = await getAxeStats('https://github.com/18F/a11y-metrics');
     expect(s.url).toBe('https://github.com/18F/a11y-metrics');
